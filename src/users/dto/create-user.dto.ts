878103b186fe613d4b0869ad2ae12465baed771a
Douglas <douglasrdv@proton.me>
2024-07-24 15:50:01 -0300

    feat: Implementing CreatedUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'This is the name of the employee to be displayed',
+    example: 'Pranay Mehrotra'
+  })
+  id: string; // uuid
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  parentUserId: string; // Can be null
+}
