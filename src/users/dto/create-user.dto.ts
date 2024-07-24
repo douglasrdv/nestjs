295c9b85a3d95392f373660bb6955a60e69c8822
Douglas <douglasrdv@proton.me>
2024-07-24 16:00:40 -0300

    config: Add description and example to swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,38 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUUID, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({
-    description: 'This is the name of the employee to be displayed',
-    example: 'Pranay Mehrotra'
-  })
-  id: string; // uuid
-
-  @ApiProperty()
-  username: string;
-
-  @ApiProperty()
-  password: string;
-
-  @ApiProperty()
-  parentUserId: string; // Can be null
+    @ApiProperty({
+        description: 'Unique identifier for the user',
+        example: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      })
+      @IsUUID()
+      @IsNotEmpty()
+      id: string;
+    
+      @ApiProperty({
+        description: 'Username of the user',
+        example: 'john_doe',
+      })
+      @IsString()
+      @IsNotEmpty()
+      username: string;
+    
+      @ApiProperty({
+        description: 'Password of the user',
+        example: 'securePassword123',
+      })
+      @IsString()
+      @IsNotEmpty()
+      password: string;
+    
+      @ApiProperty({
+        description: 'Parent user ID, if applicable',
+        example: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        required: false,
+        nullable: true,
+      })
+      @IsUUID()
+      @IsOptional()
+      parentUserId?: string | null;
 }
