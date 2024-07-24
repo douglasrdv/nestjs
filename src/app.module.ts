50a1846ade5d39960b9fe269fb21380c53fb6d7a
Douglas <douglasrdv@proton.me>
2024-07-24 16:34:12 -0300

    feat: TypeORM now communicating with PostgreSQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+import { User } from './users/entities/user.entity'
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'douglasrdv',
+      password: '323',
+      database: 'companydb',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class AppModule {}
